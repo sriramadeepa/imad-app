9f60e2d10a356700fecdf0a4201fa57e6b353eb0
deepa <30749797+sriramadeepa@users.noreply.github.com>
2017-08-08 20:59:23 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/ui/style.css', function (req, res) {
+app.get('/ sri.html', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'sri.html'));
 });
 
